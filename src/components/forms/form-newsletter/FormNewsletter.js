04e907a35975fe59379f8780d8e75a3176fbb6ef
WorldStar0126 <tomasbillion555@gmail.com>
2023-01-21 21:53:42 +0600

    fixed issue

--- src/components/forms/form-newsletter/FormNewsletter.js
+++ src/components/forms/form-newsletter/FormNewsletter.js
@@ -1,40 +1,42 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Script from 'next/script';
 import classNames from 'classnames';
 import Link from 'next/link';
 import ButtonOutline from 'src/components/buttons/button-outline/ButtonOutline';
 import InputCheckbox from 'src/components/inputs/input-checkbox/InputCheckbox';
 import InputSelect from 'src/components/inputs/input-select/InputSelect';
-
 import { isEmailValid } from 'src/utils/helpers';
 
 import styles from './FormNewsletter.module.scss';
 import InputRounded from 'src/components/inputs/input-rounded/InputRounded';
+import MailchimpSubscribe from "react-mailchimp-subscribe";
 
 export default function FormNewsletter({ className, title, submitLabel }) {
     // Form created in CreateSend, updated to work with React.
     // TODO: check a solution that uses a post with AJAX.
 
-    return (
-        <section className={className}>
-            <h3 className={styles.heading}>
-                {title}
-            </h3>
-
-            <form
-                className={classNames(styles.form, 'js-cm-form')}
-                id="mc-embedded-subscribe-form"
-                action="https://kmy.us14.list-manage.com/subscribe/post?u=17d5a5808a83460d1c551c061&amp;id=a394b97739&amp;v_id=4354&amp;f_id=00d15ce0f0"
-                method="post" 
-            >
-                <div id="mc_embed_signup_scroll">
+    const CustomForm = ({ status, message, onValidated }) => {
+        let email, name;
+        const submit = () =>
+          email &&
+          name &&
+          email.value.indexOf("@") > -1 &&
+          onValidated({
+            EMAIL: email.value,
+            NAME: name.value
+          });
+      
+        return (
+          <div className={classNames(styles.form, 'js-cm-form')}>
+                <div>
                     <InputRounded className={styles.input}>
                         <input
                             aria-label="Name"
                             placeholder="First Name"
-                            id="mce-FNAME"
+                            id="fieldName"
                             maxLength="200"
-                            name="FNAME"
+                            name="NAME"
+                            ref={node => (name = node)}
                             required={true}
                         />
                     </InputRounded>
@@ -45,40 +47,72 @@ export default function FormNewsletter({ className, title, submitLabel }) {
                             autoComplete="Email"
                             aria-label="Email"
                             placeholder="Email Address"
-                            id="mce-EMAIL"
+                            id="fieldEmail"
                             maxLength="200"
+                            ref={node => (email = node)}
                             name="EMAIL"
                             required={true}
                             type="email"
                         />
                     </InputRounded>
 
-                    <InputCheckbox className={styles.inputCheckbox}>
-                        <input
-                            aria-required={true}
-                            id="cm-privacy-consent"
-                            className="av-checkbox"
-                            name="gdpr[195642]"
-                            required={true}
-                            type="checkbox"
-                        />
-
-                        <label htmlFor="cm-privacy-consent">
-                            I agree your&nbsp; <a href="https://www.bestevaer.com/privacy-policy" target="_blank">privacy policy.</a>
+                    <InputRounded className={styles.input}>
+                        <label htmlFor="cm-privacy-consent" className='text-white'>
+                            By clicking the subscribe button, I consent to having KM Yachtbuilders store my submitted information for marketing purposes. <Link href="https://www.bestevaer.com/privacy-policy" className='text-white' target="_blank">Our privacy policy.</Link>
                         </label>
-                    </InputCheckbox>
-
+                    </InputRounded>
+                   
+                    <input
+                        id="cm-privacy-consent-hidden"
+                        name="cm-privacy-consent-hidden"
+                        type="hidden"
+                        value={true}
+                    />
                 </div>
 
                 <ButtonOutline
-                    id="mc-embedded-subscribe"
                     className={styles.button}
                     label={submitLabel}
                     type="submit"
+                    onClick={submit}
                 />
-            </form>
+                {status === "sending" && <div className="mailchimp-status">sending...</div>}
+                {status === "error" && (
+                    <div
+                        className="mailchimp-status"
+                        dangerouslySetInnerHTML={{ __html: message }}
+                    />
+                )}
+                {status === "success" && (
+                    <div
+                        className="mailchimp-status"
+                        dangerouslySetInnerHTML={{ __html: message }}
+                    />
+                )}            
+            
+          </div>
+        );
+      };
 
-            <Script src="//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js"></Script>
+    const postUrl = "https://kmy.us14.list-manage.com/subscribe/post?u=17d5a5808a83460d1c551c061&amp;id=a394b97739&amp;v_id=4354&amp;f_id=00d15ce0f0";
+    return (
+        <section className={className}>
+            <h3 className={styles.heading}>
+                {title}
+            </h3>
+            <div className="mc__form-container">
+                <MailchimpSubscribe
+                    url={postUrl}
+                    render={({ subscribe, status, message }) => (
+                        <CustomForm
+                            status={status} 
+                            message={message}
+                            onValidated={formData => subscribe(formData)}
+                        />
+                    )}
+                />
+            </div>
+
         </section>
     );
 }
